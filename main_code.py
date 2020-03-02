c5c2810c5367205d06bbe13886aeb2a7a72b9af6
Abhay SV <monuabhay@yahoo.in>
2020-03-02 10:24:51 +0530

    User-agent Change

--- main_code.py
+++ main_code.py
@@ -38,19 +38,28 @@ import json,ssl
 from pprint import pprint
 import subprocess
 import os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+#------------------------------------------------------------------------------------
 
 
 
 #--------------------[API Call AND JSON DUMP]---------------------------------------
-# url='https://monuabhaysv-eval-prod.apigee.net/svabhay2' 
-# logger.info('URL Generated :'+url)
-# a=urllib.urlopen(url,context=ctx).read().decode()
-# response=json.loads(a)
 
-p1 = subprocess.Popen('curl -k "https://www.nseindia.com/api/quote-equity?symbol=EIHOTEL" -H "authority: beta.nseindia.com" -H "cache-control: max-age=0" -H "dnt: 1" -H "upgrade-insecure-requests: 1" -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36" -H "sec-fetch-user: ?1" -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "sec-fetch-site: none" -H "sec-fetch-mode: navigate" -H "accept-encoding: gzip, deflate, br" -H "accept-language: en-US,en;q=0.9,hi;q=0.8" --compressed  -o sardard.json', shell=True)
+#Chdir used as SSL wala Curl use karna hai
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+output_file='sardard.json'
+symbol_nse='EIHOTEL'
+nse_url= '"https://www.nseindia.com/api/quote-equity?symbol='+symbol_nse+'"'
+# Headers me ched mad yehi time khata hai
+nse_headers=' -H "authority: beta.nseindia.com" -H "cache-control: max-age=0" -H "dnt: 1" -H "upgrade-insecure-requests: 1" -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36" -H "sec-fetch-user: ?1" -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "sec-fetch-site: none" -H "sec-fetch-mode: navigate" -H "accept-encoding: gzip, deflate, br" -H "accept-language: en-US,en;q=0.9,hi;q=0.8"'
+
+cmd_nse= 'curl -k '+nse_url+nse_headers+' --compressed  -o '+output_file
+# Without wait curl it reads buffer before the curl ka output aye
+p1 = subprocess.Popen(cmd_nse, shell=True)
 p1.wait()
+
+
 f=open("sardard.json","r")
 var=f.read()
 response=json.loads(var)
