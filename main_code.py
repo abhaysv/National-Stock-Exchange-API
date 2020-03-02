5c0d98365ff55d3841c11ec2f77d6751155f65e9
Abhay SV <monuabhay@yahoo.in>
2020-03-02 09:00:20 +0530

    Fixed Headers

--- main_code.py
+++ main_code.py
@@ -15,6 +15,8 @@ import datetime,time
 import urllib.request as urllib
 import hashlib 
 
+
+
 #---------------------[APP LOGGING]--------------------------------------------------
 import logging.config
 from datetime import datetime
@@ -33,20 +35,26 @@ logger.critical("THE DAEMON RUN INITIAED")
 
 #--------------------[FOR API]-------------------------------------------------------
 import json,ssl
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-api_key='5062acabf92c4007835e04cc8734a20c'
-#------------------------------------------------------------------------------------
+from pprint import pprint
+import subprocess
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 
 
 #--------------------[API Call AND JSON DUMP]---------------------------------------
-url='https://monuabhaysv-eval-prod.apigee.net/svabhay2' 
-logger.info('URL Generated :'+url)
-a=urllib.urlopen(url,context=ctx).read().decode()
-response=json.loads(a)
+# url='https://monuabhaysv-eval-prod.apigee.net/svabhay2' 
+# logger.info('URL Generated :'+url)
+# a=urllib.urlopen(url,context=ctx).read().decode()
+# response=json.loads(a)
+
+p1 = subprocess.Popen('curl -k "https://www.nseindia.com/api/quote-equity?symbol=EIHOTEL" -H "authority: beta.nseindia.com" -H "cache-control: max-age=0" -H "dnt: 1" -H "upgrade-insecure-requests: 1" -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36" -H "sec-fetch-user: ?1" -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "sec-fetch-site: none" -H "sec-fetch-mode: navigate" -H "accept-encoding: gzip, deflate, br" -H "accept-language: en-US,en;q=0.9,hi;q=0.8" --compressed  -o sardard.json', shell=True)
+p1.wait()
+f=open("sardard.json","r")
+var=f.read()
+response=json.loads(var)
+print(response)
 
 today_date = time.strftime("%m/%d/%Y")
 #------------------------------------------------------------------------------------
